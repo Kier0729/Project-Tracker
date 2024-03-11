c0497dc7e921c6e2ec49bdef1b0343a50c077136
Kier Anthony Dalit <kier0729@gmail.com>
2024-03-11 23:49:10 +0800

    modification for fetching data if no changes in year has been made

--- tracker/src/components/App.js
+++ tracker/src/components/App.js
@@ -32,7 +32,7 @@ const[selectedItem, setSelectedItem]=useState("");
 
 const date = new Date();
 const [yearList, setyearList] = useState(null);
-const [options, setOptions] = useState({cycle:7, selectedMonth:date.getMonth()+1, selectedYear:yearList[0] || date.getFullYear()})
+const [options, setOptions] = useState({cycle:7, selectedMonth:date.getMonth()+1, selectedYear:yearList ? yearList[0] : date.getFullYear()})
 
 const myHeader = {
     // withCredentials: true,
